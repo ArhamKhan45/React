d41999013c91d158b2fb46e1a6edb15cca0d5942
Arhamkhan <karham45@gmail.com>
2024-06-06 00:38:01 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+import RoutePage from "./Component/RoutePage";
+function App() {
+  return (
+    <div className="App">
+      <RoutePage />
+    </div>
+  );
+}
+
+export default App;
--- src/app/useeffect/Productdisplay.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Productdisplay() {
-  return <div></div>;
-}
-
-export default Productdisplay;
